fb4e6d8fc3348243875f64e8ca09172c3baad3c9
mendel <rhthrhrl0@naver.com>
2023-08-22 11:32:00 +0900

    feat: 디버그용 해시키 추출 코드 추가

--- Throw_fornt/app/src/main/java/com/example/throw_fornt/feature/home/HomeActivity.kt
+++ Throw_fornt/app/src/main/java/com/example/throw_fornt/feature/home/HomeActivity.kt
@@ -1,6 +1,10 @@
 package com.example.throw_fornt.feature.home
 
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
@@ -9,6 +13,9 @@ import com.example.throw_fornt.databinding.ActivityHomeBinding
 import com.example.throw_fornt.feature.map.MapFragment
 import com.example.throw_fornt.feature.myPage.MyPageFragment
 import com.example.throw_fornt.util.common.BindingActivity
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
 
 class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home) {
     private val viewModel: HomeViewModel by viewModels()
@@ -16,6 +23,7 @@ class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
         setupViewModel()
+        getHashKey() // 여기서 로그로 나온 디버그용 해시키 값을 카카오 콘솔에 등록할 것
     }
 
     private fun setupViewModel() {
@@ -39,6 +47,25 @@ class HomeActivity : BindingActivity<ActivityHomeBinding>(R.layout.activity_home
         return when (fragmentType) {
             FragmentType.MAP -> MapFragment()
             FragmentType.MY_PAGE -> MyPageFragment()
+        }
+    }
+
+    private fun getHashKey() {
+        var packageInfo: PackageInfo? = null
+        try {
+            packageInfo = packageManager.getPackageInfo(packageName, PackageManager.GET_SIGNATURES)
+        } catch (e: PackageManager.NameNotFoundException) {
+            e.printStackTrace()
+        }
+        if (packageInfo == null) Log.e("KeyHash", "KeyHash:null")
+        for (signature in packageInfo!!.signatures) {
+            try {
+                val md = MessageDigest.getInstance("SHA")
+                md.update(signature.toByteArray())
+                Log.d("KeyHash", Base64.encodeToString(md.digest(), Base64.DEFAULT))
+            } catch (e: NoSuchAlgorithmException) {
+                Log.e("KeyHash", "Unable to get MessageDigest. signature=$signature", e)
+            }
         }
     }
 }
