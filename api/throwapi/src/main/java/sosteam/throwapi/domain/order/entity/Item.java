303c590a7c30e0475b1089bad77163a6c3c002fe
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-08-25 03:43:55 +0900

    Gifticon 재고 수량에 따른 비즈니스 로직 추가

--- api/throwapi/src/main/java/sosteam/throwapi/domain/order/entity/Item.java
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/order/entity/Item.java
@@ -3,6 +3,7 @@ package sosteam.throwapi.domain.order.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
+import sosteam.throwapi.domain.order.exception.NotEnoughStockException;
 import sosteam.throwapi.global.entity.PrimaryKeyEntity;
 
 import java.util.Set;
@@ -24,4 +25,25 @@ public class Item extends PrimaryKeyEntity {
 
     @OneToMany(mappedBy = "item", fetch = FetchType.LAZY)
     private Set<Gifticon> gifticons;
+
+    private int stockQuantity;
+
+    //==비즈니스 로직==//
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity+=quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock=this.stockQuantity-quantity;
+        if(restStock<0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity=restStock;
+    }
 }
---
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/order/exception/NotEnoughStockException.java
@@ -0,0 +1,19 @@
+package sosteam.throwapi.domain.order.exception;
+
+public class NotEnoughStockException extends RuntimeException{
+    public NotEnoughStockException() {
+        super();
+    }
+
+    public NotEnoughStockException(String message) {
+        super(message);
+    }
+
+    public NotEnoughStockException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotEnoughStockException(Throwable cause) {
+        super(cause);
+    }
+}
