0b1135e30c0a9777932572ce3c7c19aa7b30ab1d
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-08-24 21:54:58 +0900

    move

--- api/throwapi/src/main/java/sosteam/throwapi/domain/order/repository/GifticonRepository.java
+++
@@ -1,9 +0,0 @@
-package sosteam.throwapi.domain.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sosteam.throwapi.domain.order.entity.Gifticon;
-
-import java.util.UUID;
-
-public interface GifticonRepository extends JpaRepository<Gifticon, UUID> {
-}
--- api/throwapi/src/main/java/sosteam/throwapi/domain/order/repository/ReceiptRepository.java
+++
@@ -1,9 +0,0 @@
-package sosteam.throwapi.domain.order.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import sosteam.throwapi.domain.order.entity.Receipt;
-
-import java.util.UUID;
-
-public interface ReceiptRepository extends JpaRepository<Receipt, UUID> {
-}
