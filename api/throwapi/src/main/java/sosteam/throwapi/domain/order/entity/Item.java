a2b8a8dacb655b186f331733f2f9ab41794b1f74
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-08-27 00:03:03 +0900

    Entity 완성

--- api/throwapi/src/main/java/sosteam/throwapi/domain/order/entity/Item.java
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/order/entity/Item.java
@@ -14,10 +14,22 @@ import java.util.List;
 public class Item extends PrimaryKeyEntity {
 
     @NotNull
-    private String name;
+    private String product_name; // 테스트상품명
 
     @NotNull
-    private Long price;
+    private String brand_name; // 테스트브랜드명
+
+    @NotNull
+    private String brand_image_url; // 테스트브랜드이미지url
+
+    @NotNull
+    private String product_image_url; // 테스트상품이미지url
+
+    @NotNull
+    private  String product_thumb_image_url; // 테스트상품썸네일url
+
+    @NotNull
+    private Long price; // 상품 가격
 
     @OneToMany(mappedBy = "item", fetch = FetchType.LAZY)
     private List<Gifticon> gifticons;
