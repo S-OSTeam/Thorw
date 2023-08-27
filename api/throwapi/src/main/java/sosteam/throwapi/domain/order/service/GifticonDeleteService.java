143d855924ff0cfec514a33987819bacb0852cc9
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-08-27 17:21:11 +0900

    주석

--- api/throwapi/src/main/java/sosteam/throwapi/domain/order/service/GifticonDeleteService.java
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/order/service/GifticonDeleteService.java
@@ -13,11 +13,16 @@ import sosteam.throwapi.domain.order.repository.repo.GifticonRepository;
 public class GifticonDeleteService {
     private final GifticonRepository gifticonRepository;
 
+    //조회
     private Gifticon getGifticonByGiftTraceId(String giftTraceId) {
         return gifticonRepository.findByGiftTraceId(giftTraceId)
                 .orElseThrow(() -> new EntityNotFoundException("Gifticon not found for given gift_trace_id"));
     }
 
+    /**
+     * 기프티콘 고유 Id를 통해 gifticon 삭제
+     * @param giftTraceId : 기프티콘 고유 Id
+     */
     public void deleteGifticonByGiftTraceId(String giftTraceId) {
         Gifticon gifticon = getGifticonByGiftTraceId(giftTraceId);
         gifticonRepository.delete(gifticon);
