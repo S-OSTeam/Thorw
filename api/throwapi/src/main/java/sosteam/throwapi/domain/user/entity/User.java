2e4adc859debe9d833346dedf4ad59cb3d9009c7
madlimeus <108660314+madlimeus@users.noreply.github.com>
2023-08-30 00:51:53 +0900

    createAt, updateAt
    add createAt, updateAt in User entity

--- api/throwapi/src/main/java/sosteam/throwapi/domain/user/entity/User.java
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/user/entity/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +16,7 @@ import sosteam.throwapi.global.entity.Role;
 import sosteam.throwapi.global.entity.SNSCategory;
 import sosteam.throwapi.global.entity.UserStatus;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -37,6 +40,13 @@ public class User extends PrimaryKeyEntity implements UserDetails {
 
     @Column(unique = true)
     private String snsId;
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt = LocalDateTime.now();
 
     @Enumerated(EnumType.STRING)
     private SNSCategory sns;
