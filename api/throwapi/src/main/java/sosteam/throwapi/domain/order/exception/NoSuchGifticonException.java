593d60c890060af0d8bf36bf84af6a24fe6f3feb
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-08-29 12:07:44 +0900

    코드는 무조건 대문자로

--- api/throwapi/src/main/java/sosteam/throwapi/domain/order/exception/NoSuchGifticonException.java
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/order/exception/NoSuchGifticonException.java
@@ -5,6 +5,6 @@ import sosteam.throwapi.global.exception.exception.CommonException;
 
 public class NoSuchGifticonException extends CommonException {
     public NoSuchGifticonException() {
-        super("No_SUCH_GIFTICON","찾고자 하는 기프티콘이 없습니다.",HttpStatus.NOT_FOUND);
+        super("NO_SUCH_GIFTICON","찾고자 하는 기프티콘이 없습니다.",HttpStatus.NOT_FOUND);
     }
 }
