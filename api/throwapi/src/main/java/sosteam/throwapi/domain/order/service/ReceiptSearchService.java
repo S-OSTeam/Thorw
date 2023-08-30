dd5f8fa1f667bed36a9f031ee13c010be8aabab3
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-08-30 13:39:58 +0900

    영수증 조회

--- api/throwapi/src/main/java/sosteam/throwapi/domain/order/service/ReceiptSearchService.java
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/order/service/ReceiptSearchService.java
@@ -3,10 +3,24 @@ package sosteam.throwapi.domain.order.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import sosteam.throwapi.domain.order.entity.Receipt;
+import sosteam.throwapi.domain.order.repository.repo.ReceiptRepository;
+
+import java.util.*;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class ReceiptSearchService {
+    private final ReceiptRepository receiptRepository;
 
+    public Optional<Receipt> getReceiptById(UUID receiptId) {
+        log.debug("RECEIPT SEARCH");
+        return receiptRepository.findById(receiptId);
+    }
+
+    public Set<Receipt> getAllReceipts() {
+        log.debug("RECEIPTS SEARCH");
+        return new HashSet<>(receiptRepository.findAll());
+    }
 }
