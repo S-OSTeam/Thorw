a9a985e7c2734f421df22a6e41002c175ec9bed6
Hangeulkim <41067036+Hangeulkim@users.noreply.github.com>
2023-08-30 14:34:02 +0900

    Update NotExistentUserInKakao.java

--- api/throwapi/src/main/java/sosteam/throwapi/domain/oauth/exception/NotExistentUserInKakao.java
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/oauth/exception/NotExistentUserInKakao.java
@@ -5,7 +5,7 @@ import sosteam.throwapi.global.exception.exception.CommonException;
 
 public class NotExistentUserInKakao extends CommonException {
     public NotExistentUserInKakao() {
-        super("NON_EXISTENT_USER_IN_KAKAO", "카카오에 등록되지 않는 사용자 입니다.", HttpStatus.UNAUTHORIZED);
+        super("NOT_EXISTENT_USER_IN_KAKAO", "카카오에 등록되지 않는 사용자 입니다.", HttpStatus.UNAUTHORIZED);
     }
 
 }
