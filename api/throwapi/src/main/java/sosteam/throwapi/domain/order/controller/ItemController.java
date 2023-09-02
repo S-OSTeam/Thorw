413502f843e52f719421696535114db2fefab67e
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-09-02 20:49:59 +0900

    사용자가 상품이름으로 검색하는 기능 추가

--- api/throwapi/src/main/java/sosteam/throwapi/domain/order/controller/ItemController.java
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/order/controller/ItemController.java
@@ -1,16 +1,21 @@
 package sosteam.throwapi.domain.order.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import sosteam.throwapi.domain.order.controller.request.ItemSearchRequest;
 import sosteam.throwapi.domain.order.controller.response.ItemResponse;
 import sosteam.throwapi.domain.order.entity.Item;
 import sosteam.throwapi.domain.order.entity.Dto.ItemDto;
 import sosteam.throwapi.domain.order.service.ItemSearchService;
 
+import java.util.Collections;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -25,9 +30,22 @@ public class ItemController {
      * Item 리스트 조회
      * @return 아이템 정보
      */
-    @PostMapping("/search")
+    @PostMapping("/search/all")
     public ResponseEntity<Set<ItemResponse>> searchItemList(){
         Set<Item> items = itemSearchService.searchAllItems();
+        Set<ItemResponse> responses = items.stream()
+                .map(ItemDto::toItemResponse)
+                .collect(Collectors.toSet());
+        return ResponseEntity.ok(responses);
+    }
+
+    /**
+     * Product 이름으로 Item 조회
+     */
+    @PostMapping("/search")
+    public ResponseEntity<Set<ItemResponse>> searchItemByProductNameContaining(@RequestBody @Valid ItemSearchRequest request){
+        Set<Item> items = itemSearchService.searchByProductNameContaining(request.getProductName())
+                .orElse(Collections.emptySet());
         Set<ItemResponse> responses = items.stream()
                 .map(ItemDto::toItemResponse)
                 .collect(Collectors.toSet());
