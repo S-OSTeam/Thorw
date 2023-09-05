eb5b845dae91b7cb21061a8fd863a47c6d13cf83
Hangeulkim <ckswls89@naver.com>
2023-09-06 01:15:46 +0900

    hotfix/createdAt-안들어가는거-수정
    hotfix/createdAt-안들어가는거-수정

--- api/throwapi/src/main/java/sosteam/throwapi/domain/user/entity/UserAuth.java
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/user/entity/UserAuth.java
@@ -1,10 +1,12 @@
 package sosteam.throwapi.domain.user.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import sosteam.throwapi.global.entity.PrimaryKeyEntity;
 
 import java.time.LocalDateTime;
@@ -12,6 +14,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class UserAuth extends PrimaryKeyEntity {
 
     @NotNull
