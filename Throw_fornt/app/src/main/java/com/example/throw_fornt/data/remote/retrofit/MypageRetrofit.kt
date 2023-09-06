c441c61556a5dcb61c27c65e1e6cb45d82f913ee
KimEuiSoo <63793652+KimEuiSoo@users.noreply.github.com>
2023-09-06 23:30:59 +0900

    feat: 가게등록 수정 삭제 완료 (#84)

--- Throw_fornt/app/src/main/java/com/example/throw_fornt/data/remote/retrofit/MypageRetrofit.kt
+++ Throw_fornt/app/src/main/java/com/example/throw_fornt/data/remote/retrofit/MypageRetrofit.kt
@@ -1,35 +1,31 @@
 package com.example.throw_fornt.data.remote.retrofit
 
 import android.util.Log
-import com.example.throw_fornt.data.model.request.Register
-import com.example.throw_fornt.data.model.request.StoreRequest
-import com.example.throw_fornt.data.model.response.StoreModel
-import com.example.throw_fornt.data.model.response.StoreResponse
-import com.google.gson.GsonBuilder
+import com.example.throw_fornt.data.model.request.MypageRequest
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
-import java.lang.Exception
 import java.net.URL
+import java.security.cert.X509Certificate
+import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.TrustManager
+import javax.net.ssl.X509TrustManager
 
-class StoreRetrofit {
+class MypageRetrofit {
     companion object {
         private const val HTTP_LOG_TAG = "HTTP_LOG"
 
-        const val apiKey = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0YWtlZmxhbWUiLCJleHAiOjE2OTQwMTIwNDgsImtpbmQiOiJhY2Nlc3NUb2tlbiJ9.Fu3iqYJ89T1i1Vzf2oHGtdRFdHgOYkQm1p1HwvlcfnWqfFqc3YYdGLJwIBqkb60Zg_tu2a6nGPYXcRQeSmlwOA"
         //가게등록, 내 가게조회, 사업자등록번호 조회를 위한 공용url
         const val url = "https://moviethree.synology.me/api/"
+        const val apiKey ="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0YWtlZmxhbWUiLCJleHAiOjE2OTQwMTIwNDgsImtpbmQiOiJhY2Nlc3NUb2tlbiJ9.Fu3iqYJ89T1i1Vzf2oHGtdRFdHgOYkQm1p1HwvlcfnWqfFqc3YYdGLJwIBqkb60Zg_tu2a6nGPYXcRQeSmlwOA"
     }
 
     //client 객체
     private val client = OkHttpClient.Builder().addInterceptor(getHttpLoggingInterceptor()).addInterceptor { chain ->
         val newRequest = chain.request().newBuilder()
-            .addHeader("Authorization", "Bearer $apiKey")
+            .addHeader("Authorization", "Bearer ${StoreRetrofit.apiKey}")
             .build()
         chain.proceed(newRequest)
     }.build()
@@ -42,7 +38,7 @@ class StoreRetrofit {
     }
 
     //url 객체
-    private val urls = URL(url)
+    private val urls = URL(StoreRetrofit.url)
 
     //retrofit 객체
     private val retrofit = Retrofit.Builder()
@@ -51,5 +47,5 @@ class StoreRetrofit {
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    val storeService = retrofit.create(StoreRequest::class.java)
+    val mypageService = retrofit.create(MypageRequest::class.java)
 }
--- Throw_fornt/app/src/main/java/com/example/throw_fornt/feature/store/StoreActivity.kt
+++ Throw_fornt/app/src/main/java/com/example/throw_fornt/feature/store/StoreActivity.kt
@@ -1,5 +1,6 @@
 package com.example.throw_fornt.feature.store
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.viewModels
@@ -12,31 +13,46 @@ import com.example.throw_fornt.feature.store.management.ManagementActivity
 import com.example.throw_fornt.feature.store.management.ManagementViewModel
 import com.example.throw_fornt.feature.store.register.RegisterActivity
 import com.example.throw_fornt.feature.store.register.RegisterViewModel
+import com.example.throw_fornt.feature.store.register.address.AddressResult
 import com.example.throw_fornt.util.common.BindingActivity
+import com.example.throw_fornt.util.common.Toaster
+import java.lang.Exception
 
 class StoreActivity : BindingActivity<ActivityStoreBinding>(R.layout.activity_store) {
     private val viewModel: StoreViewModel by viewModels()
     private val managerViewModel: ManagementViewModel by viewModels()
     var items = ArrayList<StoreModel>()
+    val REQUEST_CODE: Int = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding.viewModel = viewModel
         binding.lifecycleOwner = this
-        viewModel.event.observe(this) { changeActivity(it) }
+        try {
+            viewModel.storeList()
+        }
+        catch (e:Exception){
 
-        listData()
+        }
+        viewModel.event.observe(this) { changeActivity(it) }
     }
 
     //내 가게정보를 받는 데이터를 adapter를 통해 RecyclerView에 item을 넣어줌
-    fun listData(){
+    fun listData(storeList: ArrayList<StoreModel>?){
         var list = findViewById<RecyclerView>(R.id.store_list)
-
-        //TODO 삭제 예정
-        items.add(StoreModel("","02-820-0920","test1",37.4945402275658,126.95977107078,"000-00-00000","07027","상도동 509","101호","00100","",""))
-        items.add(StoreModel("","02-820-0920","test2",37.4945402275658,126.95977107078,"000-00-00000","07027","상도동 509","102호","10100","","",))
+        items = storeList!!
 
         //TODO 가게 정보 조회 api 넣기
+        val adapter = StoreAdapter(items, viewModel::service)
+        adapter.notifyDataSetChanged()
+
+        list.adapter = adapter
+        list.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+    }
+
+    fun blankData(){
+        var list = findViewById<RecyclerView>(R.id.store_list)
+        items = ArrayList<StoreModel>()
         val adapter = StoreAdapter(items, viewModel::service)
         adapter.notifyDataSetChanged()
 
@@ -49,18 +65,42 @@ class StoreActivity : BindingActivity<ActivityStoreBinding>(R.layout.activity_st
         when (event){
             is StoreViewModel.Event.Register -> register()
             is StoreViewModel.Event.Service -> storeService(event.item)
+            is StoreViewModel.Event.StoreList -> listData(event.item)
+            is StoreViewModel.Event.BlankList -> blankData()
         }
     }
 
     private fun register(){
         val intent: Intent = Intent(this, RegisterActivity::class.java)
-        startActivity(intent)
+        startActivityForResult(intent, REQUEST_CODE)
     }
 
-    private fun storeService(storeItem: StoreModel){
+    private fun storeService(storeItem: StoreModel) {
         val intent: Intent = Intent(this, ManagementActivity::class.java)
         //ManagementActivity에 선택한 내 가게 data 전달
-        intent.putExtra("data",storeItem)
-        startActivity(intent)
+        intent.putExtra("data", storeItem)
+        startActivityForResult(intent, REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            val receiveData = data!!.getStringExtra("data")
+            if (receiveData != null) {
+                if(receiveData.contains("삭제")){
+                    if(receiveData.contains("실패")){
+                        Toaster.showLong(this@StoreActivity, "삭제 하지 못했습니다.")
+                    }
+                    else Toaster.showLong(this@StoreActivity, "성공적으로 삭제 처리 되었습니다.")
+                }
+                else if(receiveData.contains("등록")){
+                    Toaster.showLong(this@StoreActivity, "성공적으로 등록 처리 되었습니다.")
+                }
+                else{
+                    Toaster.showLong(this@StoreActivity, "성공적으로 수정 되었습니다.")
+                }
+                viewModel.storeList()
+            }
+        }
     }
 }
