6de5c0dc06d5267392ad10ccecaf7b4b441b5208
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-09-06 23:10:15 +0900

    토큰 수정, url 수정

--- api/throwapi/src/main/java/sosteam/throwapi/domain/user/controller/RankingController.java
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/user/controller/RankingController.java
@@ -1,31 +1,31 @@
 package sosteam.throwapi.domain.user.controller;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import sosteam.throwapi.domain.user.controller.request.RankingRequest;
 import sosteam.throwapi.domain.user.controller.response.RankingResponse;
 import sosteam.throwapi.domain.user.entity.dto.user.RankingDto;
 import sosteam.throwapi.domain.user.service.MileageSearchService;
+import sosteam.throwapi.global.service.JwtTokenService;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Controller
 @Slf4j
-@RequestMapping("/ranking")
+@RequestMapping("/rank")
 @RequiredArgsConstructor
 public class RankingController {
     private final MileageSearchService mileageSearchService;
+    private final JwtTokenService jwtTokenService;
 
     @GetMapping("/leaderboard")
-    public ResponseEntity<List<RankingResponse>> createLeaderBoard(){
+    public ResponseEntity<List<RankingResponse>> createLeaderBoard() {
         log.debug("CREATELEADERBOARDCONTROLLER");
         List<RankingDto> leaderBoard = mileageSearchService.createLeaderBoard();
 
@@ -39,9 +39,14 @@ public class RankingController {
         return ResponseEntity.ok(rankingResponses);
     }
 
-    @PostMapping("/userranking")
-    public ResponseEntity<RankingResponse> searchRankingByuserId(@RequestBody @Valid RankingRequest request){
-        RankingDto rankingDto = mileageSearchService.searchUserRankingByInputId(request.getInputId());
+    @GetMapping
+    public ResponseEntity<RankingResponse> searchRankingByuserId(
+            @RequestHeader(name = "Authorization", required = true)
+            @Pattern(regexp = "^(Bearer)\s.+$", message = "Bearer [accessToken]")
+            String token) {
+        String accessToken = token.substring(7);
+        String inputId = jwtTokenService.extractSubject(accessToken);
+        RankingDto rankingDto = mileageSearchService.searchUserRankingByInputId(inputId);
 
         RankingResponse rankingResponse = new RankingResponse(
                 rankingDto.getUserName(),
