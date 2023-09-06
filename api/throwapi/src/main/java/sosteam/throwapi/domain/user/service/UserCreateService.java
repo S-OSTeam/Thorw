720fdcc42c2888514d489ab09b53e1903ff3dc27
madlimeus <108660314+madlimeus@users.noreply.github.com>
2023-09-06 14:34:05 +0900

    회원 가입시 milage 추가

--- api/throwapi/src/main/java/sosteam/throwapi/domain/user/service/UserCreateService.java
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/user/service/UserCreateService.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import sosteam.throwapi.domain.user.entity.Mileage;
 import sosteam.throwapi.domain.user.entity.User;
 import sosteam.throwapi.domain.user.entity.UserInfo;
 import sosteam.throwapi.domain.user.entity.dto.user.UserSaveDto;
@@ -42,13 +43,19 @@ public class UserCreateService {
                 dto.getEmail()
         );
 
+        Mileage mileage = new Mileage(Long.valueOf(0));
+
         //modify User's data(userinfo, role, userStatus)
         user.modifyRole(dto.getRole());
         user.modifyUserStatus(dto.getUserStatus());
 
-        //mapping User, UserInfo
+        //mapping User to UserInfo, Mileage
         user.modifyUserInfo(userInfo);
         userInfo.modifyUser(user);
+
+        user.modifyMileage(mileage);
+        mileage.modifyUser(user);
+
 
         // 이미 존재하는 회원을 추가 하려고 할 때 409 error
         User userResult = null;
