1fd5ef01648e335022251b19cd839c59aee446d3
Hangeulkim <ckswls89@naver.com>
2023-09-07 22:47:27 +0900

    userSearchService 변경 적용
    userSearchService 변경 적용

--- api/throwapi/src/main/java/sosteam/throwapi/domain/store/externalAPI/bizno/BiznoAPI.java
+++ api/throwapi/src/main/java/sosteam/throwapi/domain/store/externalAPI/bizno/BiznoAPI.java
@@ -9,35 +9,33 @@ import sosteam.throwapi.domain.store.log.CrnLog;
 import sosteam.throwapi.domain.store.log.CrnLogRepository;
 import sosteam.throwapi.domain.user.entity.User;
 import sosteam.throwapi.domain.user.entity.dto.user.UserInfoDto;
-import sosteam.throwapi.domain.user.service.UserReadService;
+import sosteam.throwapi.domain.user.service.UserSeaerchService;
 import sosteam.throwapi.global.service.IPService;
 import sosteam.throwapi.global.service.JwtTokenService;
-
-import java.util.UUID;
 
 @Slf4j
 @Component
 @RequiredArgsConstructor
 public class BiznoAPI {
     private final CrnLogRepository crnLogRepository;
-    private final UserReadService userReadService;
+    private final UserSeaerchService userSeaerchService;
     private final JwtTokenService jwtTokenService;
 
     @Value("${bizno.api.key}")
     private String apiKey;
 
-    public BiznoApiResponse confirmCompanyRegistrationNumber(String number,String accessToken) {
+    public BiznoApiResponse confirmCompanyRegistrationNumber(String number, String accessToken) {
         // 요청 사용자 정보 가져오기
         UserInfoDto userInfoDto = new UserInfoDto(
                 jwtTokenService.extractSubject(accessToken)
         );
-        User user = userReadService.searchByInputId(userInfoDto);
+        User user = userSeaerchService.searchByInputId(userInfoDto);
 
         // LOG to DB
         CrnLog crnLog = new CrnLog(IPService.getClientIP(), user.getId());
         crnLogRepository.save(crnLog);
 
-        log.debug("{},{}", IPService.getClientIP(),number);
+        log.debug("{},{}", IPService.getClientIP(), number);
         // Call Bizno-api to Confirm CompanyRegistrationNumber
         WebClient webClient = WebClient.builder()
                 .baseUrl("https://bizno.net/api/fapi?")
@@ -47,16 +45,16 @@ public class BiznoAPI {
                         .get()
                         .uri(uriBuilder ->
                                 uriBuilder
-                                        .queryParam("key",apiKey)
-                                        .queryParam("gb",1)
-                                        .queryParam("q",number)
-                                        .queryParam("type","json")
+                                        .queryParam("key", apiKey)
+                                        .queryParam("gb", 1)
+                                        .queryParam("q", number)
+                                        .queryParam("type", "json")
                                         .build()
                         )
                         .retrieve()
                         .bodyToMono(BiznoApiResponse.class)
                         .block();
-        log.debug("BIZNO-API-RESPONSE={}",response);
+        log.debug("BIZNO-API-RESPONSE={}", response);
         log.debug("API-KEY={}", apiKey);
         return response;
     }
